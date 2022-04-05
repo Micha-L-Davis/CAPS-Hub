a736f7e50b90ca94c58037e6bb180ad186cd6f95
Micha Davis <m.leigh.davis@gmail.com>
2022-04-04 21:33:21 -0700

    test vendor client event handlers

---
+++ __tests__/vendor.test.js
@@ -0,0 +1,27 @@
+
+const eventPool = require('../events.js');
+const { sendOrder, sendThanks } = require('../client-vendor');
+
+jest.mock('../events.js', () => {
+  return {
+    on: jest.fn(),
+    emit: jest.fn(),
+  };
+});
+
+describe('testing the vendor client', () => {
+
+  console.log = jest.fn();
+
+  it('should emit the pickup event with an order in the payload', () => {
+    sendOrder('fries');
+
+    expect(eventPool.emit).toHaveBeenCalledWith('PICKUP', 'fries');
+  });
+
+  it('should log a message to the console thanking the customer', () => {
+    sendThanks({ customer: 'Tester Testington' });
+
+    expect(console.log).toHaveBeenCalledWith('Thank you, Tester Testington');
+  });
+});
